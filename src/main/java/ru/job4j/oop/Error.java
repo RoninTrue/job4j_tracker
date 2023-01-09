124cf716bb0de6638770c707836ee265dd79fbd2
RoninTrue <vasenev.front@gmail.com>
2023-01-09 22:13:04 +0700

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error google = new Error(true, 404, "Not Found");
+        Error twitter = new Error(true, 200, "OK");
+        error.printInfo();
+        google.printInfo();
+        twitter.printInfo();
+    }
+}
