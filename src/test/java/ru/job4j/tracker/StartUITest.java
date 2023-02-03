f355657c14fa01a4fed02e5e2a42ff99602a93a8
RoninTrue <vasenev.front@gmail.com>
2023-02-03 19:48:00 +0700

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -126,4 +126,21 @@ class StartUITest {
                 + "1. Exit from the program" + ln
                 + "=== Exit Program ===" + ln);
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"1", "0"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[] {new ExitAction(out)};
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu." + ln
+                + "0. Exit from the program" + ln
+                + "Wrong input, you can select: 0.. 0" + ln
+                + "Menu." + ln
+                + "0. Exit from the program" + ln
+                + "=== Exit Program ===" + ln);
+    }
 }
