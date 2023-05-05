5cc75f4b31cd1e947880f0f9a0780a28a5fffa2c
RoninTrue <vasenev.front@gmail.com>
2023-05-05 20:53:11 +0700

    0. Stream API улучшения [#58167]

---
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .filter(Objects::nonNull)
+                .sorted((left, right) -> Integer.compare(right.getScope(), left.getScope()))
+                .takeWhile(st -> st.getScope() > bound)
+                .collect(Collectors.toList());
+    }
+}
