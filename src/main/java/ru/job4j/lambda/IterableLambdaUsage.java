a2179ef8911582d2524752be2d68c1ef66795cfa
RoninTrue <vasenev.front@gmail.com>
2023-05-10 20:35:42 +0700

    3. Optional - empty, of [#214711]

--- src/main/java/ru/job4j/lambda/IterableLambdaUsage.java
+++ src/main/java/ru/job4j/lambda/IterableLambdaUsage.java
@@ -4,27 +4,19 @@ import java.util.*;
 
 public class IterableLambdaUsage {
     public static void main(String[] args) {
-        String input = "Hello, my name is Sergey, and i am programmer.".replaceAll(" ", "");
+        String input = "Hello World".replaceAll(" ", "").toLowerCase();
         Map<Character, Integer> map = new HashMap<>();
         for (char symbol : input.toCharArray()) {
             map.merge(symbol, 1, (oldValue, newValue) -> oldValue + 1);
-//            Integer temp = map.get(symbol);
-//            Integer temp = map.getOrDefault(symbol, 0);
-//            map.put(symbol, temp + 1);
-//            if (temp != null) {
-//                map.put(symbol, temp + 1);
-//            } else {
-//                map.put(symbol, 1);
-//            }
         }
         int max = 0;
         char result = 0;
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if (max < entry.getValue()) {
+        for (var entry : map.entrySet()) {
+            if (entry.getValue() > max) {
                 max = entry.getValue();
                 result = entry.getKey();
             }
         }
-        System.out.println("Most used: " + result + " " + max);
+        System.out.println(result + " " + max);
     }
 }
