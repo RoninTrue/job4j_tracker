67cc5a3fe63d5ab030b85cf202b940015c3500bd
SergeyDevStart <vasenev.front@gmail.com>
2024-08-23 16:16:26 +0700

    1. Lombok. Настройка IDEA [#504888]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,18 +1,22 @@
 package ru.job4j.tracker;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    @EqualsAndHashCode.Include
     private int id;
+    @EqualsAndHashCode.Include
     private String name;
     private  LocalDateTime created = LocalDateTime.now().withNano(0);
-
-    public Item() {
-
-    }
 
     public Item(String name) {
         this.name = name;
@@ -27,43 +31,6 @@ public class Item {
         this.id = id;
         this.name = name;
         this.created = created;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id && Objects.equals(name, item.name) && Objects.equals(created, item.created);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, created);
     }
 
     @Override
